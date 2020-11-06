57d3a4f0f3cebf85e8122134a14b381f177f8800
James Hughes <james.hughes@ucl.ac.uk>
2020-11-06 15:58:50 +0000

    added write to csv functionality

--- screenshot/save_report.js
+++ screenshot/save_report.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const saveReport = async (page) => {
-	console.log("save report");
-
 	const reportButtonElement = await page.$(
 		`#root > div > div > div.sc-AxirZ.aHDei > div.buttons > button`
 	);
@@ -19,7 +18,8 @@ const saveReport = async (page) => {
 		`#root > div > div > div.sc-fzqBZW.ksbfDg.open > div`
 	);
 
-	const tableData = await page.evaluate(() => {
+	// get the headers from the first table
+	const tableHeadData = await page.evaluate(() => {
 		const thead = Array.from(
 			document.querySelectorAll(
 				"#root > div > div > div.sc-fzqBZW.ksbfDg.open > div > div.sc-fzqNJr.hSLfNI > table:nth-child(2) > thead > tr > th"
@@ -27,8 +27,42 @@ const saveReport = async (page) => {
 		);
 		return thead.map((th) => th.innerText);
 	});
+	console.log(tableHeadData);
 
-	console.log(tableData);
+	// get the rows from the first table
+	const tableRowData = await page.evaluate(() => {
+		const tbody = Array.from(
+			document.querySelectorAll(
+				"#root > div > div > div.sc-fzqBZW.ksbfDg.open > div > div.sc-fzqNJr.hSLfNI > table:nth-child(2) > tbody > tr > th"
+			)
+		);
+		return tbody.map((tr) => tr.innerText);
+	});
+	console.log(tableRowData);
+
+	// write data to csv (currently experimenting)
+	const csvWriter = createCsvWriter({
+		path: "file.csv",
+		header: [
+			{ id: "1", title: tableHeadData[0] },
+			{ id: "2", title: tableHeadData[1] },
+			{ id: "3", title: tableHeadData[2] },
+			{ id: "4", title: tableHeadData[3] },
+			{ id: "5", title: tableHeadData[4] },
+			{ id: "6", title: tableHeadData[5] },
+		],
+	});
+
+	const records = [
+		{ 1: "test1", 2: "test2", 3: "test3", 4: "test4", 5: "test5", 6: "test6" },
+		{ 1: "test1", 2: "test2", 3: "test3", 4: "test4", 5: "test5", 6: "test6" },
+	];
+
+	csvWriter
+		.writeRecords(records) // returns a promise
+		.then(() => {
+			console.log("...Done");
+		});
 };
 
 exports.saveReport = saveReport;
